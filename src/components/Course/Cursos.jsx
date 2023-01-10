b912710f8d6a58171f30ea502ca3b3974c02f3ca
Tomas Breuckmann <tomas.yoga@yahoo.com>
2023-01-10 14:12:57 -0300

    erros

--- src/components/Course/Cursos.jsx
+++ src/components/Course/Cursos.jsx
@@ -30,7 +30,7 @@ export default function Cursos() {
             Com abordagem no
             <St infos={c}> Haṭha Yoga </St>
             o curso de
-            <St infos={c}> Formação de Instrutores </St>
+            <St infos={c}> Formação de Instrutores! </St>
             conta com uma carga horária de
             <St infos={c}> 240h </St>
             e destina-se a toda pessoa interessada em conhecer mais à si mesma, e também em conhecer e ensinar esta tradição.
