292e50695724af1d0cd37f892d9b66a6c3be2f6a
Tomas Breuckmann <tomas.yoga@yahoo.com>
2023-01-12 10:22:43 -0300

    repo tests

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,10 +30,9 @@ export default function Header() {
         <Menu>
           <StLink to="/">FORMAÇÃO DE INSTRUTORES</StLink>
           {/* <StLink to="/treinamentos">TREINAMENTOS</StLink> */}
-          {/* <StLink to="/artigos">ARTIGOS</StLink> */}
+          <StLink to="/artigos">ARTIGOS</StLink>
           <StLink to="/alunos">ALUNOS</StLink>
           <StLink to="/sobre">SOBRE</StLink>
-          <StLink to="/sobre">ESTUDOS E ARTIGOS</StLink>
           <a href="#footer">
             CONTATOS
           </a>
